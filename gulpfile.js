6e6161808d487e85512fdd5e8a29bfbf6f9da6f4
alik0211 <alik0211alik@gmail.com>
2017-06-29 11:41:56 +0400

    :tada: Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const del          = require('del'),
+      gulp         = require('gulp'),
+      sass         = require('gulp-sass'),
+      babel        = require('gulp-babel'),
+      rename       = require('gulp-rename'),
+      notify       = require('gulp-notify'),
+      uglify       = require('gulp-uglify'),
+      useref       = require('gulp-useref'),
+      htmlmin      = require('gulp-htmlmin'),
+      cleanCSS     = require('gulp-clean-css'),
+      browserSync  = require('browser-sync'),
+      autoprefixer = require('gulp-autoprefixer');
+
+const reload = browserSync.reload;
+
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    },
+    port: 8888,
+    notify: false
+  });
+});
+
+gulp.task('serve:dist', ['build'], function() {
+  browserSync({
+    server: {
+      baseDir: 'dist'
+    },
+    port: 9999,
+    notify: false
+  });
+});
+
+gulp.task('sass', function() {
+  return gulp.src('app/sass/*.sass')
+    .pipe(sass()).on('error', notify.onError())
+    .pipe(autoprefixer(['last 10 versions']))
+    .pipe(gulp.dest('app/css'));
+});
+
+gulp.task('clean', function() {
+  return del.sync('dist');
+});
+
+gulp.task('build', ['clean', 'sass'], function() {
+  gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist'));
+
+  gulp.src('app/img/**/*').pipe(gulp.dest('dist/img'));
+
+  gulp.src('app/css/*.css')
+    .pipe(cleanCSS({ level: { 1: {
+      specialComments: 0
+    }}}))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/css'));
+
+  gulp.src('app/js/main.js')
+    .pipe(babel({ presets: ['es2015'] }))
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', ['sass', 'serve'], function() {
+  gulp.watch('app/*.html', reload);
+  gulp.watch('app/js/**/*.js', reload);
+  gulp.watch('app/sass/**/*.sass', ['sass', reload]);
+});
+
+gulp.task('default', ['watch']);
